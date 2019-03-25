869bb67e475fce71401fa5f43790757eb18bc9f9
AmitPr <17amitprasad@gmail.com>
2019-03-25 10:50:29 -0400

    Finished calculator

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    public static double eval(String s) {
+        String[] tokens = s.split(" ");
+        MyDeque<Double> md = new MyDeque<Double>();
+        for (String token : tokens) {
+            try {
+                Double d = Double.parseDouble(token);
+                md.addFirst(d);
+            } catch (NumberFormatException e) {
+                Double arg2 = md.removeFirst();
+                Double arg1 = md.removeFirst();
+                if(token.equalsIgnoreCase("+")){
+                    md.addFirst(arg1+arg2);
+                }else if(token.equalsIgnoreCase("-")){
+                    md.addFirst(arg1-arg2);
+                }else if(token.equalsIgnoreCase("*")){
+                    md.addFirst(arg1*arg2);
+                }else if(token.equalsIgnoreCase("/")){
+                    md.addFirst(arg1/arg2);
+                }
+            }
+        }
+        return md.removeFirst();
+    }
+}
